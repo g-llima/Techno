6fea2f676f4d4582285799fc2c21c3698ae3ffc9
Gabriel Lima <gabrielrtxon@gmail.com>
2022-02-28 10:31:42 -0300

    Added sign in component

--- src/Pages/Auth/Auth.tsx
+++ src/Pages/Auth/Auth.tsx
@@ -1,30 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./CSS/Auth.css";
 
 function Auth() {
+  const [isSignIn, setIsSignIn] = useState(false);
+
   return (
     <div className="auth">
       <div className="auth__content">
         <div className="header">
           <i className="fal fa-lock-alt"></i>
-          <h2>Sign Up</h2>
+          <h2>Sign {isSignIn ? "In" : "Up"}</h2>
         </div>
         <form action="" className="body">
-          <input type="text" placeholder="First Name*" name="firstname" />
-          <input type="text" placeholder="Last Name*" name="lastname" />
+          {!isSignIn && (
+            <>
+              <input type="text" placeholder="First Name*" name="firstname" />
+              <input type="text" placeholder="Last Name*" name="lastname" />
+            </>
+          )}
           <input type="email" placeholder="Email Address*" name="email" />
           <input type="password" placeholder="Password*" name="password" />
-          <input
-            type="password"
-            placeholder="Repeat Password*"
-            name="passwordr"
-          />
-          <button type="submit">SIGN UP</button>
+          {!isSignIn && (
+            <input
+              type="password"
+              placeholder="Repeat Password*"
+              name="passwordr"
+            />
+          )}
+          <button type="submit">SIGN {isSignIn ? "IN" : "UP"}</button>
         </form>
         <p>
-          Already have an account? <Link to="/">Sign in.</Link>
+          {isSignIn ? "Don't" : "Already"} have an account?{" "}
+          <span onClick={() => setIsSignIn(!isSignIn)}>
+            Sign {isSignIn ? "Up." : "In."}
+          </span>
         </p>
       </div>
     </div>
